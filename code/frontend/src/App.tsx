008fcd7b834f60682a9657b035ad00a261f0bc2b
Conor McGovern <conor.mcgovern32@mail.dcu.ie>
2019-12-29 21:26:14 +0000

    feat(general): add a skeleton for the frontend's code structure

---
+++ code/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <h2>
+        Hello from TASM!
+      </h2>
+    </header>
+  </div>
+);
+
+export default App;
