eb1d8d3998850ab325dec1293d332dee98c00a13
mcgovc32 <conor.mcgovern32@mail.dcu.ie>
2020-01-31 16:48:50 +0000

    feat(assembler): document each transformer's purpose

--- code/frontend/src/assembler/transform.ts
+++ code/frontend/src/assembler/transform.ts
@@ -1,13 +1,21 @@
 import * as ast from './ast';
 
+// CyclicConstantDefinition is thrown when two constants are
+// defined in terms of each other. For example,
+//
+//   1 | const x y
+//   2 | const y x
+//
 export class CyclicConstantDefinition extends Error {
-
   constructor(remaining: Map<string, string>) {
     // TODO(cmgn): A better error message.
     super(`could not expand constants, cycle in: ${remaining}`);
   }
 }
 
+// Remove all of the characters from the program (e.g. 'a'), replacing
+// them with their corresponding ASCII integers. Non-ASCII characters
+// are not supported, because we are restricted to an 8-bit word size.
 export function removeCharacters(program: ast.Node): ast.Node {
   const transformer = ast.createTransformer({
     visitCharacter: (_visitor, node, _context) => new ast.Integer(
@@ -18,6 +26,17 @@ export function removeCharacters(program: ast.Node): ast.Node {
   return program.accept(transformer, {});
 }
 
+// Remove the string definitions (ascii, asciiz), and replace them with
+// blocks of bytes. For example,
+//
+//   1| asciiz "hell0"
+//   2| ascii "hello"
+//
+// is transformed into
+//
+//   1| byte 'h' byte 'e' byte 'l' byte 'l' byte 'o' byte '\0'
+//   2| byte 'h' byte 'e' byte 'l' byte 'l' byte 'o'
+//
 export function removeStrings(program: ast.Node): ast.Node {
   const transformer = ast.createTransformer({
     visitAscii: (_visitor, node, _context) => new ast.Block(
@@ -33,11 +52,14 @@ export function removeStrings(program: ast.Node): ast.Node {
   return program.accept(transformer, {});
 }
 
+// Remove the constants from the program. This replaces all of
+// the constant identifiers with their corresponding values. If
+// a cycle is detected, then a CyclicConstantDefinition is thrown.
 export function removeConstants(program: ast.Node): ast.Node {
   interface Context {
     ready: Map<string, number>,
     unready: Map<string, string>,
-  };
+  }
   const extractConstantValue = ast.createNullableVisitor<null, { name: string, context: Context} >({
     visitInteger: (_visitor, node, { name, context }) => {
       context.ready.set(name, node.value);
@@ -50,7 +72,7 @@ export function removeConstants(program: ast.Node): ast.Node {
   });
   const collectConstantPairs = ast.createNullableVisitor({
     visitConstant: (_visitor, node, context) => {
-      node.value.accept(extractConstantValue, { name: node.name, context: context });
+      node.value.accept(extractConstantValue, { name: node.name, context });
     },
   });
   const ready = new Map<string, number>();
@@ -60,6 +82,7 @@ export function removeConstants(program: ast.Node): ast.Node {
   let moreReplacements = true;
   while (moreReplacements) {
     moreReplacements = false;
+    // eslint-disable-next-line no-loop-func
     unready.forEach((val, key) => {
       const mapping = ready.get(val);
       if (mapping !== undefined) {
