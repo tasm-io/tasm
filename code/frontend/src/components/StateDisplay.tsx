3e2a414adb4b7a9f4e5f5a76f76fa94da8358bc3
Fradl <sean.fradl@sap.com>
2020-02-07 12:46:02 +0000

    minor fixes / lint fixing
    fix CRLF line ends on some files
    removed annotations from ace editor
    began development on debugger frontend with placeholder content

--- code/frontend/src/components/StateDisplay.tsx
+++ code/frontend/src/components/StateDisplay.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../App.css';
 import RegisterDisplay from './RegisterDisplay';
-import { RegisterDisplayProps } from './RegisterDisplay';
+
 
 const StateDisplay: React.FC = () => (
   <div className="StateDisplay">
-    <div className="Row" style={{marginTop: '2em'}}>
+    <div className="Row" style={{ marginTop: '2em' }}>
       <RegisterDisplay name="AL" value="0000 0000" />
       <RegisterDisplay name="BL" value="0000 0000" />
       <RegisterDisplay name="CL" value="0000 0000" />
       <RegisterDisplay name="DL" value="0000 0000" />
     </div>
-    <div className="Row" style={{marginTop: '2em'}}>
+    <div className="Row" style={{ marginTop: '2em' }}>
       <RegisterDisplay name="IP" value="0000 0000" />
       <RegisterDisplay name="SP" value="0000 0000" />
       <RegisterDisplay name="SR" value="0000 0000" />
