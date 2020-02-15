f64b0a23d5ede99b15e1f4704da692b9c20c6d63
mcgovc32 <conor.mcgovern32@mail.dcu.ie>
2020-02-15 21:32:07 +0000

    feat(assembler): implement the remainder of the arithmetic operators

--- code/frontend/src/assembler/assemble.test.ts
+++ code/frontend/src/assembler/assemble.test.ts
@@ -1,5 +1,5 @@
 import * as ast from './ast';
-import { assembleProgram } from './assemble';
+import assemble from './assemble';
 import { Register, Opcode } from '../instructionset/instructionset';
 
 const source = { line: 0, column: 0, offset: 0 };
@@ -14,7 +14,7 @@ function buildMemory(...commands: [number, number][]): number[] {
 
 it('assembles a basic add', () => {
   expect(
-    assembleProgram(
+    assemble(
       new ast.Instruction(
         source,
         'add',
@@ -29,6 +29,65 @@ it('assembles a basic add', () => {
       [0, Opcode.ADD_REG_INT],
       [1, Register.AL],
       [2, 5],
+    ),
+  );
+});
+
+it('implements all the arithmetic operators', () => {
+  expect(
+    assemble(
+      new ast.Block(
+        source,
+        [
+          new ast.Instruction(
+            source,
+            'add',
+            [
+              new ast.Register(source, 'al'),
+              new ast.Register(source, 'al'),
+            ],
+          ),
+          new ast.Instruction(
+            source,
+            'sub',
+            [
+              new ast.Register(source, 'sp'),
+              new ast.Integer(source, 43),
+            ],
+          ),
+          new ast.Instruction(
+            source,
+            'mul',
+            [
+              new ast.Register(source, 'sp'),
+              new ast.Register(source, 'dl'),
+            ],
+          ),
+          new ast.Instruction(
+            source,
+            'div',
+            [
+              new ast.Register(source, 'bl'),
+              new ast.Register(source, 'cl'),
+            ],
+          ),
+        ],
+      ),
+    ),
+  ).toStrictEqual(
+    buildMemory(
+      [0, Opcode.ADD_REG_REG],
+      [1, Register.AL],
+      [2, Register.AL],
+      [3, Opcode.SUB_REG_INT],
+      [4, Register.SP],
+      [5, 43],
+      [6, Opcode.MUL_REG_REG],
+      [7, Register.SP],
+      [8, Register.DL],
+      [9, Opcode.DIV_REG_REG],
+      [10, Register.BL],
+      [11, Register.CL],
     ),
   );
 });
--- code/frontend/src/assembler/assemble.ts
+++ code/frontend/src/assembler/assemble.ts
@@ -1,8 +1,18 @@
 import * as ast from './ast';
-import { Register, Opcode, OpcodeMapping } from '../instructionset/instructionset';
+import {
+  Register,
+  // This import is used. I don't understand why eslint thinks that it isn't.
+  // Maybe it's because it's only used as a type?
+  // eslint-disable-next-line no-unused-vars
+  Opcode,
+  Operand,
+  OperandTypes,
+  OpcodeMapping,
+} from '../instructionset/instructionset';
 
 class State {
   private memory: number[];
+
   private wpos: number;
 
   constructor() {
@@ -24,15 +34,33 @@ class State {
   }
 }
 
-const OpcodeHandlers: { [key: string]: (instr: ast.Instruction, state: State) => void } = {
-  'add': (instr: ast.Instruction, state: State) => {
-    state.write(Opcode.ADD_REG_INT);
-    assembleOperand(instr.operands[0], state);
-    assembleOperand(instr.operands[1], state);
-  },
-};
+// TODO(cmgn): De-duplicate this. It's also in semantic.ts.
+function getOperandTypes(instruction: ast.Instruction): Operand[] {
+  const typeOf = ast.createNullableVisitor<null | Operand, {}>({
+    visitInteger: (_visitor, _node, _context) => Operand.INTEGER,
+    // An identifier represents either a label, or a constant, so we can be sure that
+    // it will eventually turn into an integer.
+    visitIdentifier: (_visitor, _node, _context) => Operand.INTEGER,
+    visitRegister: (_visitor, _node, _context) => Operand.REGISTER,
+    visitDirectAddress: (_visitor, _node, _context) => Operand.MEMORY,
+    visitRegisterAddress: (_visitor, _node, _context) => Operand.MEMORY,
+    visitRegisterOffsetAddress: (_visitor, _node, _context) => Operand.MEMORY,
+  });
+  // This can't actually return nulls, we just can't prove it to the type checker.
+  return instruction.operands.map((operand) => operand.accept(typeOf, {}) as Operand);
+}
 
-function assembleOperand(node: ast.Node, state: State) {
+function getMatchingOpcode(instr: ast.Instruction): Opcode {
+  const operandTypes = getOperandTypes(instr);
+  const possibleOpcodes = OpcodeMapping[instr.opcode];
+  return possibleOpcodes.filter((opcode) => {
+    const expectedOperandTypes = OperandTypes[opcode];
+    return (expectedOperandTypes.length === operandTypes.length) && (
+      expectedOperandTypes.every((a, i) => a === operandTypes[i]));
+  })[0];
+}
+
+function assembleOperand(operand: ast.Node, state: State) {
   const visitor = ast.createNullableVisitor({
     visitInteger: (_visitor, node, _context) => {
       state.write(node.value);
@@ -40,23 +68,46 @@ function assembleOperand(node: ast.Node, state: State) {
     visitRegister: (_visitor, node, _context) => {
       state.write(Register[node.name.toUpperCase() as keyof typeof Register]);
     },
-  });
-  node.accept(visitor, {});
-}
-
-function assembleStatement(node: ast.Node, state: State) {
-  const visitor = ast.createNullableVisitor({
-    visitInstruction: (_visitor, node, _context) => {
-      const handler = OpcodeHandlers[node.opcode];
-      handler(node, state);
+    visitDirectAddress: (_visitor, node, _context) => {
+      assembleOperand(node.value, state);
+    },
+    visitRegisterAddress: (_visitor, node, _context) => {
+      assembleOperand(node.register, state);
+    },
+    visitRegisterOffsetAddress: (_visitor, node, _context) => {
+      // TODO(cmgn): Come up with a nicer way of doing this.
+      const fakeState = new State();
+      assembleOperand(node.register, fakeState);
+      assembleOperand(node.offset, fakeState);
+      const fakeMemory = fakeState.extract();
+      // eslint-disable-next-line no-bitwise
+      state.write((fakeMemory[0] << 5) | fakeMemory[1]);
     },
   });
-  node.accept(visitor, {});
+  operand.accept(visitor, {});
 }
 
-export function assembleProgram(program: ast.Node): number[] {
+function assembleStatement(stmt: ast.Node, state: State) {
+  const visitor = ast.createNullableVisitor({
+    visitInstruction: (_visitor, node, _context) => {
+      const opcode = getMatchingOpcode(node);
+      state.write(opcode);
+      node.operands.forEach((operand) => assembleOperand(operand, state));
+    },
+    visitBlock: (_visitor, node, _context) => {
+      node.statements.forEach((subStmt) => assembleStatement(subStmt, state));
+    },
+    visitByte: (_visitor, node, _context) => {
+      // The value of this must be an integer, because it was transformed into one
+      // during the transformation stage.
+      assembleOperand(node.value, state);
+    },
+  });
+  stmt.accept(visitor, {});
+}
+
+export default function assemble(program: ast.Node): number[] {
   const state = new State();
   assembleStatement(program, state);
   return state.extract();
 }
-
