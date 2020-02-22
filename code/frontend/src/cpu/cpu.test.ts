cd30896738d0a39781609bbf5286469e2a0906f6
mcgovc32 <conor.mcgovern32@mail.dcu.ie>
2020-02-22 20:38:06 +0000

    cleanup(cpu): refactor the interpretation of instructions

--- code/frontend/src/cpu/cpu.test.ts
+++ code/frontend/src/cpu/cpu.test.ts
@@ -1,268 +1,135 @@
-import CPU from './cpu';
-import { Register } from '../instructionset/instructionset';
+import { State, fetchNextInstruction, executeInstruction } from './cpu';
+import { Opcode, Register } from '../instructionset/instructionset';
 
-test('initialize values correctly', () => {
-  const byteCode = [5, 0, 11];
-  const cpu: CPU = new CPU(byteCode);
-  const expectedRegisters = new Uint8Array(7);
-  expectedRegisters[Register.SP] = 255;
-  expect(cpu.registers).toStrictEqual(expectedRegisters);
-  expect(cpu.byteCode).toStrictEqual(byteCode);
-  expect(cpu.RAM.length).toBe(256);
+it('fetches the next nullary instruction', () => {
+  const state: State = {
+    registers: new Uint8Array(7),
+    memory: new Uint8Array([Opcode.STI]),
+  };
+  expect(fetchNextInstruction(state)).toStrictEqual([Opcode.STI, new Uint8Array([])]);
 });
 
-// Move instructions
-
-test('move instruction with register, register', () => {
-  const byteCode = [2, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[1] = 6;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(6);
+it('fetches the next unary instruction', () => {
+  const state: State = {
+    registers: new Uint8Array(7),
+    memory: new Uint8Array([Opcode.CALL, 50]),
+  };
+  expect(fetchNextInstruction(state)).toStrictEqual([Opcode.CALL, new Uint8Array([50])]);
 });
 
-test('move instruction with register, absolute memory', () => {
-  const byteCode = [3, 0, 2];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.step();
-  expect(cpu.registers[0]).toBe(2);
+it('fetches the next binary instruction', () => {
+  const state: State = {
+    registers: new Uint8Array(7),
+    memory: new Uint8Array([Opcode.ADD_REG_INT, Register.AL, 50]),
+  };
+  expect(fetchNextInstruction(state)).toStrictEqual(
+    [
+      Opcode.ADD_REG_INT,
+      new Uint8Array([Register.AL, 50]),
+    ],
+  );
 });
 
-test('move instruction with register, register memory offset', () => {
-  const byteCode = [4, 0, 38, 0, 0, 0, 0, 9];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[1] = 1;
-  cpu.step();
-  expect(cpu.offset(38)).toBe(7);
-  expect(cpu.registers[0]).toBe(9);
+it('fetches the next ternary instruction', () => {
+  const state: State = {
+    registers: new Uint8Array(7),
+    memory: new Uint8Array([Opcode.ADD_REG_REG, Register.AL, Register.BL, 0]),
+  };
+  expect(fetchNextInstruction(state)).toStrictEqual(
+    [
+      Opcode.ADD_REG_REG,
+      new Uint8Array([Register.AL, Register.BL]),
+    ],
+  );
 });
 
-test('move instruction with register, integer', () => {
-  const byteCode = [5, 0, 129];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.step();
-  expect(cpu.registers[0]).toBe(129);
+it('executes ADD_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([1, 2, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.ADD_REG_REG, new Uint8Array([Register.AL, Register.BL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([3, 2, 0, 0, 0, 0, 0]));
 });
 
-test('move instruction with absolute memory, register', () => {
-  const byteCode = [6, 0, 0];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 7;
-  cpu.step();
-  expect(cpu.RAM[0]).toBe(7);
+it('executes SUB_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([0, 1, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.SUB_REG_REG, new Uint8Array([Register.AL, Register.BL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([255, 1, 0, 0, 0, 64, 0]));
 });
 
-test('move instruction with register memory offset, register', () => {
-  const byteCode = [7, 33, 0, 0, 0, 0, 0];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 4;
-  cpu.registers[1] = 4;
-  expect(cpu.offset(33)).toBe(5);
-  cpu.step();
-  expect(cpu.RAM[5]).toBe(4);
+it('executes MUL_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([5, 5, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.MUL_REG_REG, new Uint8Array([Register.AL, Register.BL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([25, 5, 0, 0, 0, 0, 0]));
 });
 
-test('move instruction with absolute memory, integer', () => {
-  const byteCode = [8, 3, 99];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.step();
-  expect(cpu.RAM[3]).toBe(99);
+it('executes DIV_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([5, 2, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.DIV_REG_REG, new Uint8Array([Register.AL, Register.BL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([2, 2, 0, 0, 0, 0, 0]));
 });
 
-test('move instruction with register memory offset, integer', () => {
-  const byteCode = [9, 33, 123];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.step();
-  expect(cpu.RAM[1]).toBe(123);
+it('executes AND_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([5, 1, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.AND_REG_REG, new Uint8Array([Register.AL, Register.BL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([1, 1, 0, 0, 0, 0, 0]));
 });
 
-// Arithmetic
-
-test('add instruction with register, register', () => {
-  const byteCode = [10, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 2;
-  cpu.registers[1] = 4;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(6);
+it('executes OR_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([2, 0, 1, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.OR_REG_REG, new Uint8Array([Register.AL, Register.CL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([3, 0, 1, 0, 0, 0, 0]));
 });
 
-test('add instruction with register, integer', () => {
-  const byteCode = [11, 0, 11];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 2;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(13);
+it('executes XOR_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([3, 1, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.XOR_REG_REG, new Uint8Array([Register.AL, Register.BL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([2, 1, 0, 0, 0, 0, 0]));
 });
 
-test('sub instruction with register, register', () => {
-  const byteCode = [12, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 10;
-  cpu.registers[1] = 4;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(6);
+it('executes MOV_REG_REG', () => {
+  const state: State = {
+    registers: new Uint8Array([0, 0, 0, 2, 0, 0, 0]),
+    memory: new Uint8Array([]),
+  };
+  executeInstruction(state, Opcode.MOV_REG_REG, new Uint8Array([Register.AL, Register.DL]));
+  expect(state.registers).toStrictEqual(new Uint8Array([2, 0, 0, 2, 0, 0, 0]));
 });
 
-test('sub instruction with register, integer', () => {
-  const byteCode = [13, 0, 9];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 10;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(1);
+it('executes MOV_REG_MEMABS', () => {
+  const state: State = {
+    registers: new Uint8Array([0, 0, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([0, 5, 0]),
+  };
+  executeInstruction(state, Opcode.MOV_REG_MEMABS, new Uint8Array([Register.SP, 1]));
+  expect(state.registers).toStrictEqual(new Uint8Array([0, 0, 0, 0, 5, 0, 0]));
 });
 
-test('mul instruction with register, register', () => {
-  const byteCode = [14, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 10;
-  cpu.registers[1] = 4;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(40);
+it('executes MOV_REG_MEMREGOFFSET', () => {
+  const state: State = {
+    registers: new Uint8Array([1, 0, 0, 0, 0, 0, 0]),
+    memory: new Uint8Array([0, 0, 5]),
+  };
+  executeInstruction(state, Opcode.MOV_REG_MEMABS, new Uint8Array([Register.DL, (Register.AL << 5) | 0b00010]));
+  expect(state.registers).toStrictEqual(new Uint8Array([1, 0, 0, 5, 0, 0, 0]));
 });
 
-test('mul instruction with register, integer', () => {
-  const byteCode = [15, 0, 9];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 10;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(90);
-});
-
-test('div instruction with register, register', () => {
-  const byteCode = [16, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 10;
-  cpu.registers[1] = 5;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(2);
-});
-
-test('div instruction with register, integer', () => {
-  const byteCode = [17, 0, 5];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 16;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(3);
-});
-
-// Bitwise Logic
-
-test('and instruction with register, register', () => {
-  const byteCode = [18, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 240;
-  cpu.registers[1] = 60;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(48);
-});
-
-test('and instruction with register, integer', () => {
-  const byteCode = [19, 0, 60];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 240;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(48);
-});
-
-test('or instruction with register, register', () => {
-  const byteCode = [20, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.registers[1] = 48;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(240);
-});
-
-test('or instruction with register, integer', () => {
-  const byteCode = [21, 0, 48];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(240);
-});
-
-test('xor instruction with register, register', () => {
-  const byteCode = [22, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.registers[1] = 48;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(208);
-});
-
-test('xor instruction with register, integer', () => {
-  const byteCode = [23, 0, 48];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(208);
-});
-
-test('not instruction with register', () => {
-  const byteCode = [26, 0];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.step();
-  expect(cpu.registers[0]).toBe(31);
-});
-
-// Comparison
-
-test('cmp instruction with register, register', () => {
-  const byteCode = [24, 0, 1];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.registers[1] = 224;
-  cpu.step();
-  expect(cpu.registers[Register.SR]).toBe(64);
-  cpu.registers[Register.SR] = 0;
-  cpu.registers[1] = 225;
-  cpu.registers[Register.IP] = 0;
-  cpu.step();
-  expect(cpu.registers[Register.SR]).toBe(128);
-});
-
-
-test('cmp instruction with register, integer', () => {
-  const byteCode = [25, 0, 224];
-  const cpu: CPU = new CPU(byteCode);
-  cpu.registers[0] = 224;
-  cpu.step();
-  expect(cpu.registers[Register.SR]).toBe(64);
-  cpu.registers[Register.SR] = 0;
-  cpu.RAM[2] = 225;
-  cpu.registers[Register.IP] = 0;
-  cpu.step();
-  expect(cpu.registers[Register.SR]).toBe(128);
-});
-
-// utils
-
-it('sets the sign flag', () => {
-  const cpu: CPU = new CPU([]);
-  expect(cpu.registers[Register.SR]).toBe(0);
-  cpu.setSignFlag();
-  expect(cpu.registers[Register.SR]).toBe(128);
-});
-
-it('unsets the sign flag', () => {
-  const cpu: CPU = new CPU([]);
-  cpu.registers[Register.SR] = 129;
-  cpu.unsetSignFlag();
-  expect(cpu.registers[Register.SR]).toBe(1);
-});
-
-it('sets the zero flag', () => {
-  const cpu: CPU = new CPU([]);
-  expect(cpu.registers[Register.SR]).toBe(0);
-  cpu.setZeroFlag();
-  expect(cpu.registers[Register.SR]).toBe(64);
-});
-
-it('unsets the zero flag', () => {
-  const cpu: CPU = new CPU([]);
-  cpu.registers[Register.SR] = 65;
-  cpu.unsetZeroFlag();
-  expect(cpu.registers[Register.SR]).toBe(1);
-});
--- code/frontend/src/cpu/cpu.ts
+++ code/frontend/src/cpu/cpu.ts
@@ -1,339 +1,135 @@
 /* eslint-disable no-bitwise */
-import { Register, Opcode } from '../instructionset/instructionset';
+/* eslint-disable no-param-reassign */
+import { Register, Opcode, OperandTypes } from '../instructionset/instructionset';
 
-class CPU {
-    byteCode: number[]
-
-    RAM: Uint8Array
-
-    registers: Uint8Array
-
-    instructionSet: { [key: string]: () => void } = {
-      [Opcode.NOP]: () => {},
-      [Opcode.MOV_REG_REG]: this.MOV_REG_REG,
-      [Opcode.MOV_REG_MEMABS]: this.MOV_REG_MEMABS,
-      [Opcode.MOV_REG_MEMREGOFFSET]: this.MOV_REG_MEMREGOFFSET,
-      [Opcode.MOV_REG_INT]: this.MOV_REG_INT,
-      [Opcode.MOV_MEMABS_REG]: this.MOV_MEMABS_REG,
-      [Opcode.MOV_MEMREGOFFSET_REG]: this.MOV_MEMREGOFFSET_REG,
-      [Opcode.MOV_MEMABS_INT]: this.MOV_MEMABS_INT,
-      [Opcode.MOV_MEMREGOFFSET_INT]: this.MOV_MEMREGOFFSET_INT,
-      [Opcode.ADD_REG_REG]: this.ADD_REG_REG,
-      [Opcode.ADD_REG_INT]: this.ADD_REG_INT,
-      [Opcode.SUB_REG_REG]: this.SUB_REG_REG,
-      [Opcode.SUB_REG_INT]: this.SUB_REG_INT,
-      [Opcode.MUL_REG_REG]: this.MUL_REG_REG,
-      [Opcode.MUL_REG_INT]: this.MUL_REG_INT,
-      [Opcode.DIV_REG_REG]: this.DIV_REG_REG,
-      [Opcode.DIV_REG_INT]: this.DIV_REG_INT,
-      [Opcode.AND_REG_REG]: this.AND_REG_REG,
-      [Opcode.AND_REG_INT]: this.AND_REG_INT,
-      [Opcode.OR_REG_REG]: this.OR_REG_REG,
-      [Opcode.OR_REG_INT]: this.OR_REG_INT,
-      [Opcode.XOR_REG_REG]: this.XOR_REG_REG,
-      [Opcode.XOR_REG_INT]: this.XOR_REG_INT,
-      [Opcode.CMP_REG_REG]: this.CMP_REG_REG,
-      [Opcode.CMP_REG_INT]: this.CMP_REG_INT,
-      [Opcode.NOT]: this.NOT,
-      [Opcode.PUSH]: this.PUSH,
-      [Opcode.POP]: this.POP,
-    }
-
-    constructor(byteCode: number[]) {
-      this.byteCode = byteCode;
-      this.registers = new Uint8Array(7).fill(0);
-      this.registers[Register.SP] = 255;
-      this.RAM = new Uint8Array(256);
-      this.RAM.set(byteCode);
-    }
-
-    step() {
-      const instruction: number = this.RAM[this.registers[Register.IP]];
-      this.instructionSet[instruction].bind(this)();
-      this.registers[Register.IP] += 1;
-    }
-
-    // Instructions
-
-    // MOV
-
-    MOV_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] = this.registers[SECONDARY_REG];
-    }
-
-    MOV_REG_MEMABS(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const MEM = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] = this.RAM[MEM];
-    }
-
-    MOV_REG_MEMREGOFFSET(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const MEM = this.offset(this.RAM[this.registers[Register.IP]]);
-      this.registers[REG] = this.RAM[MEM];
-    }
-
-    MOV_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] = INT;
-    }
-
-    MOV_MEMABS_REG(): void {
-      this.registers[Register.IP] += 1;
-      const MEM: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const REG = this.RAM[this.registers[Register.IP]];
-      this.RAM[MEM] = this.registers[REG];
-    }
-
-    MOV_MEMREGOFFSET_REG(): void {
-      this.registers[Register.IP] += 1;
-      const MEM = this.offset(this.RAM[this.registers[Register.IP]]);
-      this.registers[Register.IP] += 1;
-      const REG = this.RAM[this.registers[Register.IP]];
-      this.RAM[MEM] = this.registers[REG];
-    }
-
-    MOV_MEMABS_INT(): void {
-      this.registers[Register.IP] += 1;
-      const MEM: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT = this.RAM[this.registers[Register.IP]];
-      this.RAM[MEM] = INT;
-    }
-
-    MOV_MEMREGOFFSET_INT(): void {
-      this.registers[Register.IP] += 1;
-      const MEM = this.offset(this.RAM[this.registers[Register.IP]]);
-      this.registers[Register.IP] += 1;
-      const INT = this.RAM[this.registers[Register.IP]];
-      this.RAM[MEM] = INT;
-    }
-
-    // Arithmetic
-
-    ADD_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] += this.registers[SECONDARY_REG];
-    }
-
-    ADD_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] += INT;
-    }
-
-    SUB_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] -= this.registers[SECONDARY_REG];
-    }
-
-    SUB_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] -= INT;
-    }
-
-    MUL_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] *= this.registers[SECONDARY_REG];
-    }
-
-    MUL_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] *= INT;
-    }
-
-    DIV_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] /= this.registers[SECONDARY_REG];
-      this.registers[REG] = Math.floor(this.registers[REG]);
-    }
-
-    DIV_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] /= INT;
-      this.registers[REG] = Math.floor(this.registers[REG]);
-    }
-
-    // Bitwise
-
-    AND_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] &= this.registers[SECONDARY_REG];
-    }
-
-    AND_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] &= INT;
-    }
-
-    OR_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] |= this.registers[SECONDARY_REG];
-    }
-
-    OR_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] |= INT;
-    }
-
-    XOR_REG_REG(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] ^= this.registers[SECONDARY_REG];
-    }
-
-    XOR_REG_INT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] ^= INT;
-    }
-
-    NOT(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      const notted = ~this.registers[REG];
-      this.registers[REG] = notted;
-    }
-
-    // Comparison
-
-    CMP_REG_REG(): void {
-      this.unsetSignFlag();
-      this.unsetZeroFlag();
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const SECONDARY_REG: number = this.RAM[this.registers[Register.IP]];
-      const test = this.registers[REG] - this.registers[SECONDARY_REG];
-      if (test === 0) {
-        this.setZeroFlag();
-      } else if (test < 0) {
-        this.setSignFlag();
-      }
-    }
-
-    CMP_REG_INT(): void {
-      this.unsetSignFlag();
-      this.unsetZeroFlag();
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[Register.IP] += 1;
-      const INT: number = this.RAM[this.registers[Register.IP]];
-      const test = this.registers[REG] - INT;
-      if (test === 0) {
-        this.setZeroFlag();
-      } else if (test < 0) {
-        this.setSignFlag();
-      }
-    }
-
-    // Stack
-
-    PUSH(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.RAM[this.registers[Register.SP]] = this.registers[REG];
-      this.registers[Register.SP] -= 1;
-    }
-
-    // Todo(Fraz): Handle stack underflows?
-    POP(): void {
-      this.registers[Register.IP] += 1;
-      const REG: number = this.RAM[this.registers[Register.IP]];
-      this.registers[REG] = this.RAM[this.registers[Register.SP]];
-      this.registers[Register.SP] += 1;
-    }
-
-    // Devices
-
-    // Jumps
-
-    // Interrupts
-
-    // Utils
-
-    setSignFlag(): void {
-      this.registers[Register.SR] |= 128;
-    }
-
-    unsetSignFlag(): void {
-      this.registers[Register.SR] &= 127;
-    }
-
-    setZeroFlag(): void {
-      this.registers[Register.SR] |= 64;
-    }
-
-    unsetZeroFlag(): void {
-      this.registers[Register.SR] &= 191;
-    }
-
-    // 8080 didn't have an interrupt flag so using |= 32 ¯\_(ツ)_/¯
-    setInterruptFlag(): void {
-      this.registers[Register.SR] |= 32;
-    }
-
-    unsetInterruptFlag(): void {
-      this.registers[Register.SR] &= 223;
-    }
-
-    offset(val: number): number {
-      const offset: number = val & 0b00011111;
-      const register: number = val >> 5;
-      const value: number = this.registers[register] + offset;
-      return value;
-    }
+export interface State {
+  registers: Uint8Array,
+  memory: Uint8Array,
 }
 
-export default CPU;
+export class Halt extends Error {}
+
+export class IllegalOpcodeError extends Error {
+  constructor(opcode: number) {
+    super(`illegal opcode ${opcode}`);
+  }
+}
+
+export class IllegalOperandError extends Error {
+  constructor(opcode: number, operands: Uint8Array) {
+    super(`illegal operand(s) for ${opcode}: ${operands}`);
+  }
+}
+
+export function fetchNextInstruction(state: State): [Opcode, Uint8Array] {
+  const pos = state.registers[Register.IP];
+  const untypedOpcode = state.memory[pos];
+  if (Opcode[untypedOpcode] === 'undefined') {
+    throw new IllegalOpcodeError(state.memory[pos]);
+  }
+  const typedOpcode = untypedOpcode as Opcode;
+  const operands = new Uint8Array(OperandTypes[typedOpcode].length);
+  for (let i = 0; i < operands.length; i += 1) {
+    operands[i] = state.memory[(pos + i + 1) % state.memory.length];
+  }
+  return [typedOpcode, operands];
+}
+
+function updateFlags(state: State, lastResult: number) {
+  // Clear the existing flags.
+  state.registers[Register.SR] &= ~(128 | 64);
+  if (lastResult === 0) {
+    // Zero
+    state.registers[Register.SR] |= 128;
+  } else if (lastResult >= 128) {
+    // Overflow
+    state.registers[Register.SR] |= 64;
+  }
+}
+
+function isRegister(value: number) {
+  return Register[value] !== 'undefined';
+}
+
+const operatorFunctionFor = {
+  [Opcode.ADD_REG_REG]: (x: number, y: number) => x + y,
+  [Opcode.SUB_REG_REG]: (x: number, y: number) => x - y,
+  [Opcode.MUL_REG_REG]: (x: number, y: number) => x * y,
+  [Opcode.DIV_REG_REG]: (x: number, y: number) => Math.floor(x / y),
+  [Opcode.AND_REG_REG]: (x: number, y: number) => x & y,
+  [Opcode.OR_REG_REG]: (x: number, y: number) => x | y,
+  [Opcode.XOR_REG_REG]: (x: number, y: number) => x ^ y,
+  [Opcode.ADD_REG_INT]: (x: number, y: number) => x + y,
+  [Opcode.SUB_REG_INT]: (x: number, y: number) => x - y,
+  [Opcode.MUL_REG_INT]: (x: number, y: number) => x * y,
+  [Opcode.DIV_REG_INT]: (x: number, y: number) => Math.floor(x / y),
+  [Opcode.AND_REG_INT]: (x: number, y: number) => x & y,
+  [Opcode.OR_REG_INT]: (x: number, y: number) => x | y,
+  [Opcode.XOR_REG_INT]: (x: number, y: number) => x ^ y,
+};
+
+export function executeInstruction(state: State, opcode: Opcode, operands: Uint8Array) {
+  // TODO(cmgn): Get rid of this giant, ugly switch statement.
+  switch (opcode) {
+  case Opcode.HALT:
+    throw new Halt();
+  case Opcode.NOP:
+    break;
+  case Opcode.MOV_REG_REG:
+    if (!isRegister(operands[0]) || !isRegister(operands[1])) {
+      throw new IllegalOperandError(opcode, operands);
+    }
+    state.registers[operands[0]] = state.registers[operands[1]];
+    break;
+  case Opcode.MOV_REG_MEMABS:
+    if (!isRegister(operands[0])) {
+      throw new IllegalOperandError(opcode, operands);
+    }
+    state.registers[operands[0]] = state.memory[operands[1]];
+    break;
+  case Opcode.MOV_REG_MEMREGOFFSET:
+    {
+      const register = operands[1] >> 5;
+      if (!isRegister(register)) {
+        throw new IllegalOperandError(opcode, operands);
+      }
+      const offset = operands[1] & 0b00011111;
+      state.registers[operands[0]] = state.memory[state.registers[register] + offset];
+    }
+    break;
+  case Opcode.ADD_REG_REG:
+  case Opcode.SUB_REG_REG:
+  case Opcode.MUL_REG_REG:
+  case Opcode.DIV_REG_REG:
+  case Opcode.AND_REG_REG:
+  case Opcode.OR_REG_REG:
+  case Opcode.XOR_REG_REG:
+    if (!isRegister(operands[0]) || !isRegister(operands[1])) {
+      throw new IllegalOperandError(opcode, operands);
+    }
+    state.registers[operands[0]] = operatorFunctionFor[opcode](
+      state.registers[operands[0]],
+      state.registers[operands[1]],
+    );
+    updateFlags(state, state.registers[operands[0]]);
+    break;
+  case Opcode.ADD_REG_INT:
+  case Opcode.SUB_REG_INT:
+  case Opcode.MUL_REG_INT:
+  case Opcode.DIV_REG_INT:
+  case Opcode.AND_REG_INT:
+  case Opcode.OR_REG_INT:
+  case Opcode.XOR_REG_INT:
+    if (!isRegister(operands[0])) {
+      throw new IllegalOperandError(opcode, operands);
+    }
+    state.registers[operands[0]] = operatorFunctionFor[opcode](
+      state.registers[operands[0]],
+      operands[1],
+    );
+    updateFlags(state, state.registers[operands[0]]);
+    break;
+  default:
+    throw new Error('uh oh!');
+  }
+}
