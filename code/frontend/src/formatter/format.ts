ea304a3bb495368bd0e5b0f371b7c56a425a53e6
mcgovc32 <conor.mcgovern32@mail.dcu.ie>
2020-03-04 12:53:27 +0000

    fix(formatter): eslint errors

--- code/frontend/src/formatter/format.ts
+++ code/frontend/src/formatter/format.ts
@@ -1,32 +1,18 @@
 import * as ast from '../assembler/ast';
 
-export function format(program: ast.Node): string {
+export default function format(program: ast.Node): string {
   const formattingVisitor = ast.createNullableVisitor<string, {}>({
     visitInteger: (_visitor, node, _context) => `${node.value}`,
     visitCharacter: (_visitor, node, _context) => `'${node.value}'`,
     visitIdentifier: (_visitor, node, _context) => `${node.name}`,
     visitRegister: (_visitor, node, _context) => `${node.name.toLowerCase()}`,
-    visitBlock: (visitor, node, _context) => {
-      return node.statements.map((child) => child.accept(visitor, {})).join('\n');
-    },
-    visitByte: (visitor, node, _context) => {
-      return `    byte ${node.value.accept(visitor, {})}`;
-    },
-    visitOrg: (visitor, node, _context) => {
-      return `\n    org ${node.addr.accept(visitor, {})}`;
-    },
-    visitLabel: (_visitor, node, _context) => {
-      return `${node.name}:`;
-    },
-    visitDirectAddress: (visitor, node, _context) => {
-      return `[${node.value.accept(visitor, {})}]`;
-    },
-    visitRegisterAddress: (visitor, node, _context) => {
-      return `[${node.register.accept(visitor, {})}]`;
-    },
-    visitRegisterOffsetAddress: (visitor, node, _context) => {
-      return `[${node.register.accept(visitor, {})} + ${node.offset.accept(visitor, {})}]`;
-    },
+    visitBlock: (visitor, node, _context) => node.statements.map((child) => child.accept(visitor, {})).join('\n'),
+    visitByte: (visitor, node, _context) => `    byte ${node.value.accept(visitor, {})}`,
+    visitOrg: (visitor, node, _context) => `\n    org ${node.addr.accept(visitor, {})}`,
+    visitLabel: (_visitor, node, _context) => `${node.name}:`,
+    visitDirectAddress: (visitor, node, _context) => `[${node.value.accept(visitor, {})}]`,
+    visitRegisterAddress: (visitor, node, _context) => `[${node.register.accept(visitor, {})}]`,
+    visitRegisterOffsetAddress: (visitor, node, _context) => `[${node.register.accept(visitor, {})} + ${node.offset.accept(visitor, {})}]`,
     visitInstruction: (visitor, node, _context) => {
       const operands = node.operands.map((operand) => operand.accept(visitor, {})).join(', ');
       if (operands.length !== 0) {
@@ -34,15 +20,9 @@ export function format(program: ast.Node): string {
       }
       return `    ${node.opcode}`;
     },
-    visitAscii: (_visitor, node, _context) => {
-      return `    ascii "${node.value}"`;
-    },
-    visitAsciiz: (_visitor, node, _context) => {
-      return `    asciiz "${node.value}"`;
-    },
-    visitConstant: (visitor, node, _context) => {
-      return `    const ${node.name} ${node.value.accept(visitor, {})}`
-    },
+    visitAscii: (_visitor, node, _context) => `    ascii "${node.value}"`,
+    visitAsciiz: (_visitor, node, _context) => `    asciiz "${node.value}"`,
+    visitConstant: (visitor, node, _context) => `    const ${node.name} ${node.value.accept(visitor, {})}`,
   });
   return program.accept(formattingVisitor, {}) || '';
 }
